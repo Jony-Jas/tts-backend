1f6ae8714648e748e03f31a1e5763811a612c824
Jony-Jas <jonyjas@gmail.com>
2021-09-19 11:57:58 +0530

    added router initialization

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const router = require('./routes/router');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(router);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
   })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
